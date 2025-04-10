82b4900356abb30b30e732f6b629b7fb07be77ba
ganeshbharade <ganeshbharade08@gmail.com>
2025-04-10 10:37:18 +0530

    goa commit

--- first.py
+++ first.py
@@ -3,9 +3,10 @@ a = "this is new line writed by me"
 b = "this line is writen by openworldbranch"
 c = "this line again written by github"
 c = "this is line is writen by me through github"
-
+d = 'stashed saved here'
 
 g = "changes made by brach"
 f = "this is second stash"
 k = "this is another stach cat"
 l = "shash 1"
+n = "this is usa to goa stash"
